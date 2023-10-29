138f2cb07352eac970d2fe9aaec6939a2484dd54
C4ptainHook <boykostudyinkpi@gmail.com>
2023-10-30 00:27:13 +0200

    Merging seems to work

--- src/NaturalMerging/Algorithm/RecordSorter.cs
+++ src/NaturalMerging/Algorithm/RecordSorter.cs
@@ -29,8 +29,9 @@
                 } while (!marker.Item2);
             }
         }
-        private void Merge()
+        private int Merge()
         {
+            int numberOfRuns = 0;
             sharedBuffer = new Buffer(Constants.GenBufferSize, 2);
             using (var mainFileBridge = new Transmitter(fileName, sharedBuffer))
             using (var AFileBridge = new Transmitter(AFile, sharedBuffer))
@@ -47,68 +48,72 @@
                     sharedBuffer.NextReserved();
                     BFileBridge.PassRecord();
                     sharedBuffer.ClearReservedMarker();
-                }
-                while (!FAE && !FBE)
-                {
-                    AER = false;
-                    BER = false;
-                    switch (mainFileBridge.CompareRecords(false))
-                    {
-                        case 0:
-                            {
-                                bridgeResponse = AFileBridge.PassRecord();
-                                AER = bridgeResponse.Item1;
-                                FAE = bridgeResponse.Item2;
-                                break;
-                            }
-                        case 1:
-                            {
-                                sharedBuffer.NextReserved();
-                                bridgeResponse = BFileBridge.PassRecord();
-                                sharedBuffer.ClearReservedMarker();
-                                BER = bridgeResponse.Item1;
-                                FBE = bridgeResponse.Item2;
-                                break;
-                            }
+                
 
-                    }
-
-                    while (!AER && BER)
+                    while (!FAE && !FBE)
                     {
-                        mainFileBridge.CompareRecords(true);
-                        bridgeResponse = AFileBridge.PassRecord();
-                        AER = bridgeResponse.Item1;
-                        FAE = bridgeResponse.Item2;
-                    }
-                    while (AER && !BER)
-                    {
-                        sharedBuffer.NextReserved();
-                        mainFileBridge.CompareRecords(true);
-                        bridgeResponse = BFileBridge.PassRecord();
-                        sharedBuffer.ClearReservedMarker();
-                        BER = bridgeResponse.Item1;
-                        FBE = bridgeResponse.Item2;
-                    }
-                }
-                mainFileBridge.Write();
+                        AER = false;
+                        BER = false;
+                        switch (mainFileBridge.CompareRecords(false))
+                        {
+                            case 0:
+                                {
+                                    bridgeResponse = AFileBridge.PassRecord();
+                                    AER = bridgeResponse.Item1;
+                                    FAE = bridgeResponse.Item2;
+                                    break;
+                                }
+                            case 1:
+                                {
+                                    sharedBuffer.NextReserved();
+                                    bridgeResponse = BFileBridge.PassRecord();
+                                    sharedBuffer.ClearReservedMarker();
+                                    BER = bridgeResponse.Item1;
+                                    FBE = bridgeResponse.Item2;
+                                    break;
+                                }
 
-                while (!FAE && FBE)
-                {
-                    sharedBuffer.ClearReservedMarker();
-                    mainFileBridge.CompareRecords(true);
-                    bridgeResponse = AFileBridge.PassRecord();
-                    FAE = bridgeResponse.Item2;
-                }
-                while (FAE && !FBE)
-                {
-                    sharedBuffer.NextReserved();
-                    mainFileBridge.CompareRecords(true);
-                    bridgeResponse = BFileBridge.PassRecord();
-                    sharedBuffer.ClearReservedMarker();
-                    FBE = bridgeResponse.Item2;
-                }
-                if (!sharedBuffer.IsEmpty) mainFileBridge.Write();
+                        }
+
+                        while (!AER && BER)
+                        {
+                            mainFileBridge.CompareRecords(true);
+                            bridgeResponse = AFileBridge.PassRecord();
+                            AER = bridgeResponse.Item1;
+                            FAE = bridgeResponse.Item2;
+                        }
+                        while (AER && !BER)
+                        {
+                            sharedBuffer.NextReserved();
+                            mainFileBridge.CompareRecords(true);
+                            bridgeResponse = BFileBridge.PassRecord();
+                            sharedBuffer.ClearReservedMarker();
+                            BER = bridgeResponse.Item1;
+                            FBE = bridgeResponse.Item2;
+                        }
+                        numberOfRuns++;
+                    }
+                    mainFileBridge.Write();
             }
+
+            while (!FAE && FBE)
+            {
+                sharedBuffer.ClearReservedMarker();
+                mainFileBridge.CompareRecords(true);
+                bridgeResponse = AFileBridge.PassRecord();
+                FAE = bridgeResponse.Item2;
+            }
+            while (FAE && !FBE)
+            {
+                sharedBuffer.NextReserved();
+                mainFileBridge.CompareRecords(true);
+                bridgeResponse = BFileBridge.PassRecord();
+                sharedBuffer.ClearReservedMarker();
+                FBE = bridgeResponse.Item2;
+            }
+            if (!sharedBuffer.IsEmpty) mainFileBridge.Write();
+            }
+            return numberOfRuns;
         }
 
         public RecordSorter(string fileName)
@@ -118,7 +123,7 @@
 
         public void Sort()
         {
-            Merge();
+            Console.WriteLine("Runs in Merge\n"+Merge() + '\n' ); 
             //Distribute();
         }
     }
